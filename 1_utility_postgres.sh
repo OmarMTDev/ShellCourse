666440d36b40f07c373c3b0aa92bebef4d8a44f3
OmarMTDev <omar@tendril.us>
2024-08-04 09:38:53 -0600

    Adding shell scripts

---
+++ 1_utility_postgres.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+# Program to automate some Postgres utilities for Linux.
+echo "Starting bash programming course"
+
---
+++ 2_variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Variables
+age=29
+name=Omar
+
+echo "$name is $age years old."
