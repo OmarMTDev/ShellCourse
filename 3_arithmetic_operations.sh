d587e44d158eac42473e028ec8eadb4e26c692a7
OmarMTDev <omar@tendril.us>
2024-08-04 21:04:18 -0600

    Updating operators

--- 3_arithmetic_operations.sh
+++ 3_arithmetic_operations.sh
@@ -14,3 +14,9 @@ echo "A times B: " $((varA * varB))
 echo "Division of A divided by B: " $((varA / varB))
 echo "Residual: of A+B % A:" $(((varA+varB+7)%varA)) echo "Is equals 1. because 9+3+7/9 -> 19/9 or 9x2+1"
 
+echo "Relation operators"
+echo "A > B" $((varA > varB))
+echo "A < B" $((varA < varB))
+echo "A <= B" $((varA <= varB))
+echo "A >= B" $((varA >= varB))
+echo "A != B" $((varA != varB))
